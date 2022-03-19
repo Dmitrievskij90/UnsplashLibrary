9120da6cd972524128b3d965676bcbbd0cd0ea1d
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-03-19 19:52:13 +0600

    create PhotosViewController

---
+++ UnsplashLibrary/UnsplashLibrary/PhotosViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UnsplashLibrary
+//
+//  Created by Konstantin Dmitrievskiy on 19.03.2022.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
+
