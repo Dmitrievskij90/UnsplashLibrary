d0c3466b850f19118196381ca561aef86a1c5693
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-03-19 19:52:24 +0600

    create FavouritePhotoViewController

---
+++ UnsplashLibrary/UnsplashLibrary/FavouritePhotoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritePhotoViewController.swift
+//  UnsplashLibrary
+//
+//  Created by Konstantin Dmitrievskiy on 19.03.2022.
+//
+
+import UIKit
+
+class FavouritePhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
