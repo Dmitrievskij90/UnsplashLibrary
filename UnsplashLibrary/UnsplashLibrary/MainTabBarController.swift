0910aeaaa4a7dd5e191a31f64bc2777bcc41784b
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-03-19 19:52:43 +0600

    create MainTabBarController

---
+++ UnsplashLibrary/UnsplashLibrary/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  UnsplashLibrary
+//
+//  Created by Konstantin Dmitrievskiy on 19.03.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [createNavController(viewController: PhotosViewController(), title: "Photos", imageName: "photo.on.rectangle"),
+                           createNavController(viewController: FavouritePhotoViewController(), title: "Favourite", imageName: "heart")
+        ]
+    }
+
+    private func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.image = UIImage(systemName: imageName)
+        UITabBar.appearance().tintColor = .black
+
+        return navController
+    }
+}
