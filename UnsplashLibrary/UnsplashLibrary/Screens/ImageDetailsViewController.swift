57b283cb405b761552f0dbb73005483af61451a3
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-03-28 22:55:09 +0600

    create ImageDetailsViewController

---
+++ UnsplashLibrary/UnsplashLibrary/Screens/ImageDetailsViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ImageDetailsViewController.swift
+//  UnsplashLibrary
+//
+//  Created by Konstantin Dmitrievskiy on 28.03.2022.
+//
+
+import UIKit
+
+class ImageDetailsViewController: UIViewController {
+
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .lightGray
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    let image: UIImage
+
+    init(image: UIImage) {
+        self.image = image
+        super.init(nibName: nil, bundle: nil)
+        modalPresentationStyle = .overFullScreen
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        imageView.image = image
+    }
+
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .white
+        self.view = view
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        view.addGestureRecognizer(tapGestureRecognizer)
+
+        view.addSubview(imageView)
+        imageView.backgroundColor = .gray
+        imageView.snp.makeConstraints { make in
+            make.leading.trailing.centerY.centerX.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(2)
+        }
+    }
+
+    @objc func imageTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
