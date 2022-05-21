2bf25242535a472eb693df51aafacca988ee0d43
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-05-21 17:20:23 +0600

    add FavouritePhotoPresenterProtocol protocol

--- UnsplashLibrary/UnsplashLibrary/Screens/Favourite screen/Protocols/FavouriteScreenProtocols.swift
+++ UnsplashLibrary/UnsplashLibrary/Screens/Favourite screen/Protocols/FavouriteScreenProtocols.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import CoreData
+
+protocol FavouritePhotoViewProtocol: NSFetchedResultsControllerDelegate {
+    func reloadData()
+    func refresh()
+}
+
+protocol FavouritePhotoPresenterProtocol {
+    var view: FavouritePhotoViewProtocol? {get set}
+    var dataManager: DataBaseManagerProtocol {get set}
+    var fetchResultController: NSFetchedResultsController<FavouritePhoto>! {get set}
+    var selectedPhotos: [FavouritePhoto] {get set}
+
+    func viewWillApperar()
+    func deleteBarButtonTapped()
+    func selectBarButtonTapped()
+}
