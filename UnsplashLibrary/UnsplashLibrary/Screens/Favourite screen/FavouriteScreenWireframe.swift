98a1e68d57ad25f89e9b7b1fa1cf7f13744db6c6
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-05-22 22:23:33 +0600

    assembling the module in FavouriteScreenWireframe

--- UnsplashLibrary/UnsplashLibrary/Screens/Favourite screen/FavouriteScreenWireframe.swift
+++ UnsplashLibrary/UnsplashLibrary/Screens/Favourite screen/FavouriteScreenWireframe.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol FavouriteScreenWireframeProtocol: AnyObject {
+//  func presentImageDetailsViewController(with photo: FavouritePhoto,from view: UIViewController)
+    static func createFavouriteScreenModule(with: FavouritePhotoViewController)
+}
+
+class FavouriteScreenWireframe: FavouriteScreenWireframeProtocol {
+    class func createFavouriteScreenModule(with view: FavouritePhotoViewController) {
+        let presenter = FavouritePhotoPresenter()
+
+        view.presenter = presenter
+        view.presenter.dataManager = DataBaseManager()
+        view.presenter.wireframe = FavouriteScreenWireframe()
+        view.presenter.view = view
+    }
+}
